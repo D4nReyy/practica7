b92e9addbfbe4c35ed78730af88c9656c1d3243d
D4nReyy <123983205+D4nReyy@users.noreply.github.com>
2024-02-10 14:51:51 +0500

    Add files via upload

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << "Факториал " << i << " = " << factorial(i) << std::endl;
+    }
+
+    return 0;
+}
